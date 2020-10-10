10ca41837cc9a070c8f5e4cadce51852137339c9
Saptarshi Basu <123saptarshi.basu@gmail.com>
2020-10-10 23:22:58 +0530

    Updating index.html,App.jsx,App.css and adding custom favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import headerimage from "./assets/courseyard-banner.png";
+
+const Header=()=>{
+  return (
+    <div className="header">
+      <img className="header-banner" src={headerimage} alt="courseyard-banner" />
+    </div>
+  )
+  
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   );
 }
