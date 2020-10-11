50615d7e784e1f24df703142e08d01cb42d43f37
Saptarshi Basu <123saptarshi.basu@gmail.com>
2020-10-11 15:16:55 +0530

    Frontend updated..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import headerimage from "./assets/courseyard-banner.png";
 
-const Header=()=>{
+const Header = () => {
   return (
-    <div className="header">
-      <img className="header-banner" src={headerimage} alt="courseyard-banner" />
-    </div>
+    <header className="navbar sticky-top navbar-light bg-white shadow-sm w-auto">
+      <img className="header-img" src={headerimage} alt="courseyard-banner" height="20" />
+    </header>
   )
   
+}
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="first-row">
+        <div className="hero-title container float-left"><span class="title" role="text">A handpicked collection of best free learning resources on the Planet <span role="img">🌎</span>, uh you can also say on the Universe <span role="img">😋</span>.</span><h6>Made with ❤ by Team Coffee</h6></div>
+      </div>
+      <div><img className="image float-right" src="https://cdn.devdojo.com/images/september2020/grow-together.png" height="350"/></div>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Hero />
     </div>
   );
 }
