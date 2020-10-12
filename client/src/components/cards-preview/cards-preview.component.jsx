c36909e8cffda42ceddb09411c2c5ec264da6e4c
milan090 <milanmuhammed1@gmail.com>
2020-10-12 19:09:05 +0530

    Added postgresql support

--- client/src/components/cards-preview/cards-preview.component.jsx
+++ client/src/components/cards-preview/cards-preview.component.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CourseCard from "../card/card.component";
 import { Grid } from "@material-ui/core";
 
-import { data } from "../../test-data/data";
+const CardsPreview = () => {
+  const [courses, setCourses] = useState([]);
 
-const CardsPreview = () => (
-  <div style={{ margin: "20px" }}>
-    <Grid container spacing={5} justify="space-evenly" alignItems="center">
-      {data.map((item, i) => (
-        <Grid
-          item
-          sm={6}
-          md={4}
-          lg={3}
-          key={i}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <CourseCard
-            image={item.thumbnail}
-            name={item.name}
-            description={item.description}
-            instructor={item.instructor}
-            tags={item.tags}
-            link={item.link}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  </div>
-);
+  useEffect(() => {
+    axios
+      .get("/api/courses")
+      .then((response) => {
+        console.log(response);
+        setCourses(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [CardsPreview]);
+  return (
+    <div style={{ margin: "20px" }}>
+      <Grid container spacing={5} justify="space-evenly" alignItems="center">
+        {courses.map((item, i) => (
+          <Grid
+            item
+            sm={6}
+            md={4}
+            lg={3}
+            key={i}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <CourseCard
+              image={item.imageurl}
+              name={item.name}
+              description={item.description}
+              instructor={item.instructor}
+              tags={item.tags}
+              link={item.link}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
 export default CardsPreview;
