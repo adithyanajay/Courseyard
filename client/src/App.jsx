d6614dac3491d25db53efe8d81e5ce62123b3c91
milan090 <milanmuhammed1@gmail.com>
2020-10-13 21:18:35 +0530

    added react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-key */
-/* eslint-disable react/prop-types */
 import React from "react";
-import "./App.css";
-import Herosection from "./components/hero-section/hero-section.component";
+import { Route, Switch } from "react-router-dom";
+
 import Navbar from "./components/navbar/navbar.component";
-import CardPreview from "./components/cards-preview/cards-preview.component";
+import HomePage from "./pages/home/home.component";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Herosection />
-      <CardPreview />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/pages/home/home.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Herosection from "../../components/hero-section/hero-section.component";
+import CardPreview from "../../components/cards-preview/cards-preview.component";
+
+const HomePage = () => (
+  <div>
+    <Herosection />
+    <CardPreview />
+  </div>
+);
+
+export default HomePage;
